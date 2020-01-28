a0f172345d0784c4f3111679846ae737bc97d144
Niko1803 <nicolas.korb03@htl-salzburg.ac.at>
2020-01-28 15:14:02 +0100

    PushAssets

---
+++ Assets/MyGame/Scripts/WormController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WormController : MonoBehaviour
+{
+    public int x;
+    public string text;
+
+    // Start is called before the first frame update
+    void Start()
+
+    {
+        if (x > 0)
+        {
+            Debug.Log("x ist größer als null");
+        }
+        else
+        {
+            Debug.Log("x ist kleiner gleich null");
+        }
+
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public float AddNumbers(float a, float b)
+    {
+        float c = a + b;
+        return c;
+    }
+}
