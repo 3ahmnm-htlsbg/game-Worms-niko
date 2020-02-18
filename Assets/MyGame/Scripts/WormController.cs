d9276393b63a0dd1d648ee1f679a5902a52166c9
Niko1803 <nicolas.korb03@htl-salzburg.ac.at>
2020-02-18 15:02:54 +0100

    BeginnBewegung

--- Assets/MyGame/Scripts/WormController.cs
+++ Assets/MyGame/Scripts/WormController.cs
@@ -6,34 +6,33 @@ public class WormController : MonoBehaviour
 {
     public int x;
     public string text;
+    public float thrust = 1.0f;
+    public Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
-
     {
-        if (x > 0)
-        {
-            Debug.Log("x ist größer als null");
-        }
-        else
-        {
-            Debug.Log("x ist kleiner gleich null");
-        }
-
+        rb = GetComponent<Rigidbody>();
     }
+
+    void FixedUpdate()
+    {
+        rb.AddForce(transform.forward * thrust);
+    }
+
 
 
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown("a"))
+        {
+            print("a key was pressed");
+        }
 
     }
 
 
-    public float AddNumbers(float a, float b)
-    {
-        float c = a + b;
-        return c;
-    }
+  
 }
