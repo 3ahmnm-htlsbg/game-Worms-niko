af2d548cc0c1ea86e5aa03616759132006fbdb9e
Niko1803 <nicolas.korb03@htl-salzburg.ac.at>
2020-06-26 08:44:31 +0200

    Worm2Korb

---
+++ Worm2Korb/Assets/MyGame/Scripts/HealthManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthManager : MonoBehaviour
+{
+    public int health = 10;
+    public Text text;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "DamageZone")
+        {
+            health = health - 4;
+           Debug.Log("Kollision DamageZone findet statt");
+            Destroy(collision.gameObject);
+        }
+        else
+        {
+            Debug.Log("Andere Kollision");
+        
+        }
+        text.text = health.ToString();
+    
+    }
+}
---
+++ Worm2Korb/Assets/MyGame/Scripts/WormController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WormController : MonoBehaviour
+{
+
+    private Rigidbody rbA;
+    private Rigidbody rbB;
+    public GameObject PlayerA;
+    public GameObject PlayerB;
+    public GameObject projectile;
+
+
+    public void Start()
+    {
+        rbA = PlayerA.GetComponent<Rigidbody>();
+        rbB = PlayerB.GetComponent<Rigidbody>();
+        PlayerA = GameObject.FindGameObjectWithTag("PlayerA");
+        PlayerB = GameObject.FindGameObjectWithTag("PlayerB");
+    }
+    [SerializeField] private GameObject spawnCubeA;
+    [SerializeField] private GameObject BazookaA;
+    [SerializeField] private GameObject spawnCubeB;
+    [SerializeField] private GameObject BazookaB;
+
+
+    public void Update()
+    {
+        if (PlayerA == true)
+        {
+            if (Input.GetKey(KeyCode.A))
+            {
+                rbA.AddForce(-1.5f, 0f, 0f, ForceMode.Impulse);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                rbA.AddForce(1.5f, 0f, 0f, ForceMode.Impulse);
+            }
+            if (Input.GetKey(KeyCode.W))
+            {
+                rbA.AddForce(0f, 1.5f, 0f, ForceMode.Impulse);
+            }
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                GameObject clone = Instantiate(projectile, spawnCubeA.transform.position, Quaternion.identity);
+                Rigidbody rbProjectile = clone.GetComponent<Rigidbody>();
+                rbProjectile.AddForce(BazookaA.transform.up * 200f);
+            }
+        }
+        if (PlayerB == true)
+        {
+            if (Input.GetKey(KeyCode.J))
+            {
+                rbB.AddForce(-1.5f, 0f, 0f, ForceMode.Impulse);
+            }
+            if (Input.GetKey(KeyCode.L))
+            {
+                rbB.AddForce(1.5f, 0f, 0f, ForceMode.Impulse);
+            }
+            if (Input.GetKey(KeyCode.I))
+            {
+                rbB.AddForce(0f, 1.5f, 0f, ForceMode.Impulse);
+            }
+            if (Input.GetKeyDown(KeyCode.O))
+            {
+                GameObject clone = Instantiate(projectile, spawnCubeB.transform.position, Quaternion.identity);
+                Rigidbody rbProjectile = clone.GetComponent<Rigidbody>();
+                rbProjectile.AddForce(BazookaB.transform.up * 200f);
+            }
+        }
+    }
+}
